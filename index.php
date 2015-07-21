c217cc01801564a20781f0085b5cf2fcc51f6a6e
Jake Hartnell <JakeHartnell@hypothes.is>
2015-07-21 11:16:01 -0700

    Add userLoop (a.k.a. expert-loop) to people's plugin.

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: People
+Description: Declares a plugin that will create a custom post type displaying people.
+Version: 1.0
+Author: Jehan Tremback
+License: GPLv2
+*/
+
+require_once('lib/peoplePosts.php');
+require_once('lib/usersLoop.php');
+
+?>
---
+++ lib/usersLoop.php
@@ -0,0 +1,88 @@
+<?php
+
+  function expertLoop() {
+    $args = array(
+      'blog_id'      => $GLOBALS['blog_id'],
+      'role'         => 'Scientist',
+      'meta_key'     => '',
+      'meta_value'   => '',
+      'meta_compare' => '',
+      'meta_query'   => array(),
+      'include'      => array(),
+      'exclude'      => array(),
+      'orderby'      => 'last_name',
+      'order'        => 'ASC',
+      'offset'       => '',
+      'search'       => '',
+      'number'       => '',
+      'count_total'  => false,
+      'fields'       => 'all_with_meta',
+      'who'          => ''
+    );
+
+    $blogusers = get_users( $args );
+
+    //Open markup
+    $output = '';
+    
+    foreach ( $blogusers as $user ) {
+
+      $userdata = get_userdata( $user->get('ID') );
+      $all_meta_for_user = get_user_meta( $user->get('ID') );
+      
+      if ( !empty($all_meta_for_user['expertise'][0]) ) {
+        $expertise = $all_meta_for_user['expertise'][0];
+      }
+      if ( !empty($userdata['user_url'][0]) ) {
+        $website = $userdata['user_url'][0];
+      }    
+      if ( !empty($all_meta_for_user['orchid'][0]) ) {
+        $orchid = $all_meta_for_user['orchid'][0];
+      }
+      if ( !empty($all_meta_for_user['hypothesis'][0]) ) {
+        $hypothesis = $all_meta_for_user['hypothesis'][0];
+      }
+      if ( !empty($all_meta_for_user['title'][0]) ) {
+        $title = $all_meta_for_user['title'][0];
+      }
+      if ( !empty($all_meta_for_user['laboratory'][0]) ) {
+        $laboratory = $all_meta_for_user['laboratory'][0];
+      }
+      if ( !empty($all_meta_for_user['primaryinstitution'][0]) ) {
+        $primaryinstitution = $all_meta_for_user['primaryinstitution'][0];
+      }
+      if ( !empty($all_meta_for_user['secondaryinstitution'][0]) ) {
+        $secondaryinstitution = $all_meta_for_user['secondaryinstitution'][0];
+      }
+      if ( !empty($all_meta_for_user['publicationone'][0]) ) {
+        $publicationone = $all_meta_for_user['publicationone'][0];
+      }
+      if ( !empty($all_meta_for_user['first_name'][0]) ) {
+        $first_name = $all_meta_for_user['first_name'][0];
+      }
+      if ( !empty($all_meta_for_user['last_name'][0]) ) {
+        $last_name = $all_meta_for_user['last_name'][0];
+      }
+
+      
+      $output .='<div class="row expert">
+          <div class="med-left">
+            '.get_avatar( $user->get('ID'), $size = '256', $default = '<path_to_url>' ).'
+          </div>
+          <div class="med-body">
+            <h3 class="noborder"> <a target="_blank" href="'.$website.'">'.$first_name.' '.$last_name.'</a></h3>
+            <p>'.$title.', '.$primaryinstitution.'</p>
+            <p><small>Expertise:</small> '.$expertise.'</p>
+            <p><small>Hypothesis:</small> <a target="_blank" href="https://hypothes.is/stream?q=user:'.$hypothesis.'" class="">'.$hypothesis.'</a></p>
+          </div>
+        </div>';
+    }
+
+    //Close and return markup
+    return $output;
+  }
+
+  add_shortcode('expert-loop', 'expertLoop');
+
+?>
+
